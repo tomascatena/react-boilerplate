76cff45b20bcbc31eaa1a100d8e07b8b43ddca5b
Tomás Catena <tomascatena@gmail.com>
2022-08-07 17:37:29 +0200

    add AppBar and Switch for dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from '@mui/system';
 import { ThemeProvider } from '@mui/material/styles';
+import { useDarkMode } from './hooks/useDarkMode';
+import AboutPage from './pages/AboutPage/AboutPage';
 import CssBaseline from '@mui/material/CssBaseline';
+import HomePage from './pages/HomePage/HomePage';
 import React from 'react';
-import darkTheme from './themes/darkTheme';
-import lightTheme from './themes/lightTheme';
+import ResponsiveAppBar from '@/components/AppBar/AppBar';
 
 const App = () => {
+  const { theme, darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
 
-        <div>
-          <h1>App Test</h1>
-        </div>
+        <ResponsiveAppBar
+          darkMode={darkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
 
-        <Routes>
-          <Route
-            path="/"
-            element={<h1>Home</h1>}
-          />
-          <Route
-            path="/about"
-            element={<h1>About</h1>}
-          />
-        </Routes>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage />}
+            />
+            <Route
+              path="/about"
+              element={<AboutPage />}
+            />
+          </Routes>
+        </Container>
       </ThemeProvider>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 };
 
---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,31 @@
+import { useMediaQuery } from '@mui/material';
+import React from 'react';
+import darkTheme from '@/themes/darkTheme';
+import lightTheme from '@/themes/lightTheme';
+
+export const useDarkMode = () => {
+  // Get OS-level preference for dark mode
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  // state: boolean ; true == use dark mode
+  const [darkMode, setDarkMode] = React.useState(true);
+
+  const theme = React.useMemo(
+    () => darkMode ? darkTheme : lightTheme,
+    [darkMode]
+  );
+
+  const toggleDarkMode = (checked: boolean) => {
+    if (checked === null) {
+      setDarkMode(prefersDarkMode);
+    } else {
+      setDarkMode(checked);
+    }
+  };
+
+  return {
+    theme,
+    darkMode,
+    toggleDarkMode,
+  };
+};
