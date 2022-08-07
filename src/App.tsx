45a42f7a827692725524488eb952de40927d9e4c
Tomás Catena <tomascatena@gmail.com>
2022-08-07 15:48:07 +0200

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import darkTheme from './themes/darkTheme';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
 
-      <div>
-        <h1>App Test</h1>
-      </div>
-    </ThemeProvider>);
+        <div>
+          <h1>App Test</h1>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
