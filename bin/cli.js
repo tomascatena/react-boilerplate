49d1cc05b97271ac6c4d11945d9c29ffc76f9bf1
Tomás Catena <tomascatena@gmail.com>
2022-10-15 13:25:00 +0200

    add cli to clone repo

---
+++ bin/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+
+const runCommand = command => {
+  try {
+    execSync(command, { stdio: 'inherit' });
+  } catch (error) {
+    console.error(`Failed to execute command: ${command}`, error);
+    return false;
+  }
+
+  return true;
+};
+
+const repoName = process.argv[2];
+
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/tomascatena/react-boilerplate ${repoName}`;
+
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) {
+  console.error('Failed to clone the repository');
+  process.exit(1);
+}
+
+const installedDependencies = runCommand(installDepsCommand);
+if (!installedDependencies) {
+  console.error('Failed to install dependencies');
+  process.exit(1);
+}
+
+console.log('Done!');
+console.log(`cd into the ${repoName} directory and run npm start to start the development server`);
